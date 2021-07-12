73f165e0d7ceb01e17cc4b72e6fb0b772c9c6e77
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-07-12 19:37:45 +0530

    event-loop-done

--- node_internals/loop.js
+++ node_internals/loop.js
@@ -1,3 +1,5 @@
+// This is only a fake process as event loop or threads working in NodeJS
+
 // Lifecycle of NodeJS Application
 // From Begining to exit back to terminal
 
@@ -9,12 +11,20 @@ const pendingOperations = []
 function shouldContinue(){
     // check one: Any pending setTimeout, setInterval and setImmediate?
     // check two: Any pending OS tasks
-    // check three: Any pendinglong running operations? (fs module)
+    // check three: Any pending long running operations? (fs module)
 
     return pendingTimers.length || pendingOSTasks || pendingOperations
 }
 
 // eventLoop in NodeJS is like while loop, if the condition is falsy then it will exit to the terminal not stop
-while(shouldContinue){
-    // 
+while(shouldContinue()){
+    // 1. checking pending timers
+    // 2. checking pending OS tasks
+    // 3. Pause execution.
+    //  - a new pendingOSTask is done...
+    //  - a new pendingOperation is done
+    //  - a timer is about to complete
+    // 4. Call immediate timer
+
+    // 5. Handle any "Close" event
 }
