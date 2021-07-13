28da7df39aa1e4e7fa1a44eb287898711ff6e252
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-07-13 10:56:18 +0530

    threadpool-multithreading

--- node_internals/threads.js
+++ node_internals/threads.js
@@ -21,3 +21,7 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('5: ', Date.now() - start)
 })
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('6: ', Date.now() - start)
+})
