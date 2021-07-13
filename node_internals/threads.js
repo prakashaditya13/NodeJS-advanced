445573899105fbb5ff59fb32d3cf620ccec67b20
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-07-13 09:01:40 +0530

    test-thread-time

--- node_internals/threads.js
+++ node_internals/threads.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto')
 
+// The result of time function may be different according to your CPU 
 const start = Date.now()
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('1: ', Date.now() - start)
@@ -7,4 +8,16 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('2: ', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3: ', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4: ', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5: ', Date.now() - start)
 })
