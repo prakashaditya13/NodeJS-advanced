dd64df0da074829c50fa7656a6d8f63fae9b5826
Aditya Prakash <prakashaditya13011999@gmail.com>
2021-07-14 17:58:01 +0530

    node-cluster

--- node_performances/index.js
+++ node_performances/index.js
@@ -1,5 +1,15 @@
 // performing cluster and worker threads
-const express = require('express')
+const cluster = require('cluster')
+
+if(cluster.isMaster){
+    // index.js file executed again but in child mode
+    cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+    // cluster.fork()
+}else{
+    // child mode is executed just like a node server 
+    const express = require('express')
 const app = express()
 
 function doWork(duration){
@@ -13,6 +23,12 @@ app.get('/', (req, res) => {
     res.send("Hi there!")
 })
 
+app.get('/fast', (req,res) => {
+    res.send("This is fast!")
+})
+
 app.listen(3000)
 
 // The PORT number should always be less than 65536
+}
+
